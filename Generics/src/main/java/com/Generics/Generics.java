3fe9db6314809d9a98dbf415fb48b1a03dd1d3d0
nikhil tirpude <nikhil.tirpude317@gmail.com>
2021-05-14 01:22:47 +0530

    Refactored code to print maximum value of arguments on console(STDout)

--- Generics/src/main/java/com/Generics/Generics.java
+++ Generics/src/main/java/com/Generics/Generics.java
@@ -12,8 +12,20 @@ public class Generics <E extends Comparable<E>> {
 
     public <E extends Comparable<E>> E getMax(){
         Arrays.sort(values);
+        E max = (E) values[values.length -1];
+        printMax(max);
+
         return (E) values[values.length -1];
 
+    }
+
+    public <E> void printMax(E max) {
+        System.out.println("The maximum value is :"+ max);
+    }
+
+    public static void main(String[] args) {
+        Generics G = new Generics(1,2,3,4);
+        System.out.println(G.getMax());
     }
 
 }
